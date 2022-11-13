db88a93f14b9f6cf5497d65836a8aa6acb28c5de
Paez11 <victorpaez117@gmail.com>
2022-11-13 19:32:30 +0100

    paso a torre

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,9 @@ module es.iesfranciscodelosrios.red {
     requires com.dlsc.formsfx;
     requires validatorfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.logging;
+    requires java.xml.bind;
+    requires java.sql;
 
     opens es.iesfranciscodelosrios.red to javafx.fxml;
     exports es.iesfranciscodelosrios.red;
