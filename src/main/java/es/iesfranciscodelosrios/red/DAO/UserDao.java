036a37c6955bf2610f49863dfd3ea90bcbe76555
Paez11 <victorpaez117@gmail.com>
2022-11-15 11:03:11 +0100

    dudosamente userDAO terminado

--- src/main/java/es/iesfranciscodelosrios/red/DAO/UserDao.java
+++ src/main/java/es/iesfranciscodelosrios/red/DAO/UserDao.java
@@ -11,12 +11,18 @@ import java.util.List;
 public class UserDao extends User implements Dao {
     private static Connection con = null;
 
-    private final static String INSERT = "INSERT INTO User(name) VALUES (?)";
-    private final static String DELETE = "DELETE FROM User WHERE id=?";
-    private final static String UPDATE = "UPDATE User SET name=?, password=?, avatar=? WHERE id=?";
-    private final static String SELECTBYID = "SELECT id,name,password,avatar WHERE id=?";
-    private final static String SELECTALL = "SELECT id,name,avatar  FROM User";
-
+    private final static String INSERT = "INSERT INTO user(name) VALUES (?)";
+    private final static String DELETE = "DELETE FROM user WHERE id=?";
+    private final static String UPDATE = "UPDATE user SET name=?, password=?, avatar=? WHERE id=? FROM user";
+    private final static String SELECTBYID = "SELECT id,name,password,avatar WHERE id=? FROM user";
+    private final static String SELECTBYNAME = "SELECT id,name,password,avatar WHERE name=? FROM user";
+    private final static String SELECTALL = "SELECT id,name,avatar FROM user";
+    private final static String FOLLOW = "INSERT INTO follower VALUES (?,?)";
+    private final static String UNFOLLOW = "DELETE FROM follower WHERE id_follower=? AND id_follow=?";
+    private final static String GETFOLLOWS = "SELECT id,name,avatar FROM user WHERE id IN (SELECT id_follow FROM follower WHERE id_follower=?)";
+    private final static String GETFOLLOWERS = "SELECT id,name,avatar FROM user WHERE id IN (SELECT id_follower FROM follower WHERE id_follow=?)";
+    //private final static String GETFOLLOWS = "SELECT u.id,u.name,u.avatar FROM user u JOIN follower f ON u.id=f.id_follow WHERE u.id=?";
+    //private final static String GETFOLLOWERS = "SELECT u.id,u.name,u.avatar FROM user u JOIN follower f ON u.id=f.id_follower WHERE u.id=?";
 
     public UserDao(int id, String name){
         super(id,name);
@@ -128,7 +134,34 @@ public class UserDao extends User implements Dao {
         }
         return user;
     }
-    private List<User> getAlUsers(){
+
+    private User getByName(String name){
+        UserDao user = new UserDao(id,name,password,avatar);
+        con = Connect.getConnect();
+        if (con != null){
+            PreparedStatement st = null;
+            try {
+                st = con.prepareStatement(SELECTBYNAME);
+                st.setString(2,name);
+                if (st.execute()){
+                    ResultSet rs = st.getResultSet();
+                    if (rs.next()){
+                        name=rs.getString(2);
+                        this.id = rs.getInt("id");
+                        this.password = rs.getString("password");
+                        this.avatar = rs.getString("avatar");
+                    }
+                    rs.close();
+                }
+                st.close();
+            } catch (SQLException e) {
+                Log.severe("No se ha podido realizar la consulta getByName en la base de datos");
+            }
+        }
+        return user;
+    }
+
+    private List<User> getAllUsers(){
         List<User> users = null;
         con = Connect.getConnect();
         if (con != null){
@@ -150,5 +183,75 @@ public class UserDao extends User implements Dao {
             }
         }
         return users;
+    }
+
+    private void follow(User u){
+        con = Connect.getConnect();
+        if (con != null){
+            PreparedStatement st = null;
+            try {
+                st = con.prepareStatement(FOLLOW);
+                st.setInt(2,this.id);
+                st.setInt(1,u.getId());
+                st.executeUpdate();
+                st.close();
+            } catch (SQLException e) {
+                Log.severe("No se ha podido realizar la consulta follow en la base de datos");
+            }
+        }
+    }
+
+    private void unFollow(User u){
+        if (id==-1){
+            con = Connect.getConnect();
+            if (con != null){
+                PreparedStatement st = null;
+                try {
+                    st = con.prepareStatement(UNFOLLOW);
+                    st.setInt(1,this.id);
+                    st.setInt(2,u.getId());
+                    if (st.executeUpdate()==1){
+                        this.id=-1;
+                    }
+                    st.close();
+                } catch (SQLException e) {
+                    Log.severe("No se ha podido realizar la consulta unfollow en la base de datos");
+                }
+            }
+        }
+    }
+
+    private List<User> getFollowers(){
+        List<User> followers = null;
+        con = Connect.getConnect();
+        if (con != null){
+            PreparedStatement st = null;
+            try {
+                st = con.prepareStatement(GETFOLLOWERS);
+                st.setInt(1,this.id);
+                followers=getAllUsers();
+                st.close();
+            } catch (SQLException e) {
+                Log.severe("No se ha podido realizar la consulta getFollowers en la base de datos");
+            }
+        }
+        return followers;
+    }
+
+    private List<User> getFollows(){
+        List<User> follows = null;
+        con = Connect.getConnect();
+        if (con != null){
+            PreparedStatement st = null;
+            try {
+                st = con.prepareStatement(GETFOLLOWERS);
+                st.setInt(1,this.id);
+                follows=getAllUsers();
+                st.close();
+            } catch (SQLException e) {
+                Log.severe("No se ha podido realizar la consulta getFollows en la base de datos");
+            }
+        }
+        return follows;
     }
 }
