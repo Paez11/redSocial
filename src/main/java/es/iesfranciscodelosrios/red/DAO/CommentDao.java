f36ba29db86f302286d111e7efb4f7a3f4516d2c
Miguel Ángel <miguelcaslp@gmail.com>
2022-11-16 21:13:14 +0100

    commentDAO

--- src/main/java/es/iesfranciscodelosrios/red/DAO/CommentDao.java
+++ src/main/java/es/iesfranciscodelosrios/red/DAO/CommentDao.java
@@ -1,4 +1,115 @@
 package es.iesfranciscodelosrios.red.DAO;
 
-public class CommentDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import es.iesfranciscodelosrios.red.model.Comment;
+import es.iesfranciscodelosrios.red.model.Post;
+import es.iesfranciscodelosrios.red.model.User;
+import es.iesfranciscodelosrios.red.utils.Connection.Connect;
+
+public class CommentDao extends Comment {
+	 private final static String INSERT = "INSERT INTO comments (id_user,texto,id_post) VALUES (?,?,?)";
+	 private final static String SELECTBYUSERPOST = "SELECT id,id_user,texto,id_post FROM comments where id_user=? and id_post=?";
+	 private final static String SELECTBYPOST = "SELECT id,id_user,texto,id_post FROM comments where id_post=?";
+	 
+	
+	public CommentDao(int id,User userComment, String textComment, Post post) {
+		super(id,userComment,textComment,post);
+	}
+	public CommentDao(User userComment, String textComment, Post post) {
+		super(userComment,textComment,post);
+	}
+	
+	
+	public void saveComment() {
+        if(this.getId()==-1) {
+            //INSERT
+            Connection cn = Connect.getConnect();
+            if(cn != null) {
+                PreparedStatement ps;
+                try {
+                    ps = cn.prepareStatement(INSERT,Statement.RETURN_GENERATED_KEYS);
+                    ps.setInt(1, this.getUserComment().getId());
+                    ps.setString(2, this.getTextComment());
+                    ps.setInt(3, this.getPost().getId());
+                    ps.executeUpdate();  //devuelve 1 si ha salido bien
+                    ResultSet rs = ps.getGeneratedKeys();
+                    if(rs.next()) {
+                        this.setId(rs.getInt(1));
+                    }
+                    ps.close();
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+	
+	public static List<Comment> getAllByUser(User userByS,Post postByS ){
+        List<Comment> result = new ArrayList<Comment>();
+        Connection cn = Connect.getConnect();
+        if(cn != null) {
+            PreparedStatement ps;
+            try {
+                ps = cn.prepareStatement(SELECTBYUSERPOST);
+                ps.setInt(1,userByS.getId());
+                ps.setInt(2,postByS.getId());
+                
+                if(ps.execute()) {
+                    ResultSet rs = ps.getResultSet();
+                    while(rs.next()) {
+                        int id=rs.getInt("id");
+                        User user = new User( rs.getInt("id_user"));
+                        String texto = rs.getString("texto");
+                        Post post= new Post(rs.getInt("id_post"));
+                        result.add(new Comment(id,user, texto, post));
+                           
+                    }
+                    rs.close();
+                }
+                ps.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+	
+	public static List<Comment> getAllByPost(Post postByS ){
+        List<Comment> result = new ArrayList<Comment>();
+        Connection cn = Connect.getConnect();
+        if(cn != null) {
+            PreparedStatement ps;
+            try {
+                ps = cn.prepareStatement(SELECTBYPOST);
+                ps.setInt(1,postByS.getId());
+                
+                if(ps.execute()) {
+                    ResultSet rs = ps.getResultSet();
+                    while(rs.next()) {
+                        int id=rs.getInt("id");
+                        User user = new User( rs.getInt("id_user"));
+                        String texto = rs.getString("texto");
+                        Post post= new Post(rs.getInt("id_post"));
+                        result.add(new Comment(id,user, texto, post));
+                           
+                    }
+                    rs.close();
+                }
+                ps.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
 }
+
+
--- src/main/java/es/iesfranciscodelosrios/red/model/Comment.java
+++ src/main/java/es/iesfranciscodelosrios/red/model/Comment.java
@@ -3,26 +3,57 @@ package es.iesfranciscodelosrios.red.model;
 import java.util.Objects;
 
 public class Comment {
-    private String UserComment;
+	private int id;
+	private User userComment;
+    private String textComment;
     private Post post;
 
-    public String getUserComment() {
-        return UserComment;
-    }
+	public Comment(int id, User userComment, String textComment, Post post) {
+		this.id = id;
+		this.userComment = userComment;
+		this.textComment = textComment;
+		this.post = post;
+	}
+	public Comment(User userComment, String textComment, Post post) {
+		this.id = -1;
+		this.userComment = userComment;
+		this.textComment = textComment;
+		this.post = post;
+	}
 
-    public void setUserComment(String userComment) {
-        UserComment = userComment;
-    }
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public Post getPost() {
-        return post;
-    }
+	public User getUserComment() {
+		return userComment;
+	}
 
-    public void setPost(Post post) {
-        this.post = post;
-    }
+	public void setUserComment(User userComment) {
+		this.userComment = userComment;
+	}
 
-    @Override
+	public String getTextComment() {
+		return textComment;
+	}
+
+	public void setTextComment(String textComment) {
+		this.textComment = textComment;
+	}
+
+	public Post getPost() {
+		return post;
+	}
+
+	public void setPost(Post post) {
+		this.post = post;
+	}
+
+	@Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
